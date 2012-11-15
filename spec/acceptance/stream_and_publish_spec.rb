b381c9022bdb6ea81e8c9d3d50eda799ee34427a
andruby <andrew@bedesign.be>
2012-11-15 01:07:08 +0100

    First spec

---
+++ spec/acceptance/stream_and_publish_spec.rb
@@ -0,0 +1,38 @@
+require 'spec_helper'
+require 'net/http'
+
+describe "Streaming and publishing" do
+  before(:all) do
+    @host = '127.0.0.1'
+    @port = rand(10000)+10000
+    @channel = "randomized:test:#{rand(2**32)}"
+    @app = nil
+    @thread = Thread.new do
+      @app = Thin::Server.new(@host, @port, Juglite::App.new)
+      @app.start
+    end
+    sleep(0.01) until @app && @app.running?
+  end
+
+  after(:all) do
+    @app.stop!
+    @thread.join
+  end
+
+  it "should keep a GET open" do
+    Net::HTTP.start(@host, @port) do |http|
+      request = Net::HTTP::Get.new("/?channel=#{@channel}")
+
+      body = ""
+      http.request(request) do |response|
+        response.read_body do |chunk|
+          body << chunk
+          body.should include(": registered to channels: #{@channel}")
+          http.finish
+          break
+        end
+        break
+      end
+    end
+  end
+end

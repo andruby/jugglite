42f6968e79c50b118787e6a2c9382827397e193a
andruby <andrew@bedesign.be>
2012-12-04 18:20:25 -0800

    Add support for Remy's EventSource Polyfill through long polling

--- lib/jugglite/sse_connection.rb
+++ lib/jugglite/sse_connection.rb
@@ -4,10 +4,16 @@ module Jugglite
 
     def initialize(request)
       @request = request
+      @longpolling = request.xhr?
       @body = DeferrableBody.new
     end
 
+    def longpolling?
+      @longpolling
+    end
+
     def write(message, options = {})
+      reset_timeout if longpolling?
       buffer = ""
       options.each { |k, v| buffer << "#{k}: #{v}\n" }
       message.each_line { |line| buffer << "data: #{line.strip}\n" }
@@ -33,6 +39,15 @@ module Jugglite
 
     def errback(&block)
       @body.errback(&block)
+    end
+
+    private
+    def reset_timeout
+      # From http://html5doctor.com/server-sent-events/#using-the-polyfill
+      @timeout.cancel if @timeout
+      @timeout = EventMachine::Timer.new(0.25) do
+        self.close
+      end
     end
   end
 end
